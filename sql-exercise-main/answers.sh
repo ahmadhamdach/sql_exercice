bf05f2bdbf8fadae3912faaa9ce9165f60c4d1e4
Hamdash <ahmadhamdach1997@gmail.com>
2021-03-19 16:10:13 +0200

    basic queries

---
+++ sql-exercise-main/answers.sh
@@ -0,0 +1,9 @@
+Basic Queries
+
+"SELECT Name FROM students"
+"SELECT * FROM students WHERE Age > 30"
+"SELECT Name FROM students WHERE Gender = "F" AND Age = 30"
+"SELECT Points FROM students WHERE Name="Alex" "
+"INSERT INTO students VALUES (12, "ahmad", 24, "M", 200)"
+"UPDATE students SET Points = Points + 100 WHERE Name = "Basma" "
+"UPDATE students SET Points = Points - 50 WHERE Name = "Alex"  "
